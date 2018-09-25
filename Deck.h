e6e5820dcd4a670471b27f9a0e451246dd63bda4
shashikumar1998 <36881029+shashikumar1998@users.noreply.github.com>
2018-09-24 21:17:18 -0700

    Add files via upload

---
+++ Deck.h
@@ -0,0 +1,17 @@
+#ifndef DECK_H
+#define DECK_H
+#include "Card.h"
+class Deck {
+private:
+	Card storage[52];
+	int top = 0;
+	int index;
+public:
+	Deck();
+	void refreshDeck();
+	Card deal(); 
+	void shuffle();
+	int CardsLeft();
+	void showDeck();
+};
+#endif 
